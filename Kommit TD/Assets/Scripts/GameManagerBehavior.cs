461c05964bfe54bbda9490201727bb4e63d6ce2e
VictorChenJ <vict8639@gmail.com>
2022-01-18 19:27:14 +0100

    0.3
    Placeable Towers, Upgradable Towers, Placing and upgrading costs Bitcoins.

---
+++ Kommit TD/Assets/Scripts/GameManagerBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManagerBehavior : MonoBehaviour {
+public Text BitcoinLabel;
+
+private int Bitcoin;
+public int BITCOIN {
+  get
+  { 
+    return Bitcoin;
+  }
+  set
+  {
+    Bitcoin = value;
+    BitcoinLabel.GetComponent<Text>().text = "BITCOINS: " + Bitcoin;
+  }
+}
+
+void Start(){
+
+	BITCOIN = 1000;
+
+}
+
+}
---
+++ Kommit TD/Assets/Scripts/Place_Tower.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Place_Tower : MonoBehaviour 
+{
+
+	public GameObject turretPrefab;
+	private GameObject turret;
+
+	//Søger for at man ikke kan placere flere end 1 tower på et spot.
+	private bool CanPlaceTower () {
+  		int cost = turretPrefab.GetComponent<TowerData>().levels[0].cost;
+		return turret == null && gameManager.BITCOIN >= cost;
+
+	 }
+
+
+//1
+void OnMouseUp()
+{
+  //2
+  if (CanPlaceTower())
+  {
+    //3
+    turret = (GameObject) 
+      Instantiate(turretPrefab, transform.position, Quaternion.identity);
+    //4
+    AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+    audioSource.PlayOneShot(audioSource.clip);
+    gameManager.BITCOIN -= turret.GetComponent<TowerData>().CurrentLevel.cost;
+
+
+  }
+
+	else if (CanUpgradeTower())
+	{
+ 	 	turret.GetComponent<TowerData>().IncreaseLevel();
+ 		 AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+ 	 	audioSource.PlayOneShot(audioSource.clip);
+  		gameManager.BITCOIN -= turret.GetComponent<TowerData>().CurrentLevel.cost;
+
+	}
+
+}
+
+private bool CanUpgradeTower()
+{
+  if (turret != null)
+  {
+    TowerData TowerData = turret.GetComponent<TowerData>();
+    TowerLevel nextLevel = TowerData.GetNextLevel();
+    if (nextLevel != null)
+    {
+      return gameManager.BITCOIN >= nextLevel.cost;
+    }
+  }
+  return false;
+}
+
+private GameManagerBehavior gameManager;
+
+void Start(){
+	gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+
+
+}
+
+}
