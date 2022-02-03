619bed10ed2c0c08cdbba24c6a47e6e7d90aa21b
VictorChenJ <vict8639@gmail.com>
2022-02-03 18:24:02 +0100

    0.6

--- COMPLET/Assets/GameManagerBehavior.cs
+++ COMPLET/Assets/GameManagerBehavior.cs
@@ -46,7 +46,7 @@ public class GameManagerBehavior : MonoBehaviour
         set
         {
             gold = value;
-            goldLabel.GetComponent<Text>().text = "GOLD: " + gold;
+            goldLabel.GetComponent<Text>().text = "BITCOINS: " + gold;
         }
     }
 
